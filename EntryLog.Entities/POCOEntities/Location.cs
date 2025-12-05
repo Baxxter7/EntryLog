2bae6be986ed37fe07e87dddb6ea89227bbf04ec
Josue Martinez <josueisaiasmartinezlopez@gmail.com>
2025-12-05 15:12:57 -0600

    Add Location entity

---
+++ EntryLog.Entities/POCOEntities/Location.cs
@@ -0,0 +1,9 @@
+﻿namespace EntryLog.Entities.POCOEntities;
+
+public class Location
+{
+    public string Latitude { get; set; } = string.Empty;
+    public string Longitude { get; set; } = string.Empty;
+    public string? ApproximateAddress { get; set; }
+    public string IpAddress { get; set; } = string.Empty;
+}
