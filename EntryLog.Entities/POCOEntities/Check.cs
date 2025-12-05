9158a6191a53805adeb887c2766a73593ed54069
Josue Martinez <josueisaiasmartinezlopez@gmail.com>
2025-12-05 15:15:30 -0600

    Add Check entity

---
+++ EntryLog.Entities/POCOEntities/Check.cs
@@ -0,0 +1,11 @@
+﻿namespace EntryLog.Entities.POCOEntities;
+
+public class Check
+{
+    public string Method { get; set; } = string.Empty;
+    public string DeviceName { get; set; } = string.Empty;
+    public DateTime Date { get; set; }
+    public Location Location { get; set; } = new();
+    public string PhotoUrl { get; set; } = string.Empty;
+    public string? Notes { get; set; } 
+}
