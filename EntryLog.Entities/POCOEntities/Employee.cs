5374fe9d371c3e3beb74f19fae4a063dc277468a
Josue Martinez <josueisaiasmartinezlopez@gmail.com>
2025-12-04 20:38:04 -0600

    feat: add employee entity

---
+++ EntryLog.Entities/POCOEntities/Employee.cs
@@ -0,0 +1,12 @@
+﻿namespace EntryLog.Entities.POCOEntities;
+
+public class Employee
+{
+    public int Code { get; set; }
+    public string FullName { get; set; } = string.Empty;
+    public string Position { get; set; } = string.Empty;
+    public int OrganizationID { get; set; }
+    public string BranchOffice { get; set; } = string.Empty;
+    public string TownName { get; set; } = string.Empty;
+    public string CostCenter { get; set; } = string.Empty;
+}
