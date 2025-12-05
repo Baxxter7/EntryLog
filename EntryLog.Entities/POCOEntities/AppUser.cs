41e90a9aa90730df2717a7bf9c11155291652f4c
Josue Martinez <josueisaiasmartinezlopez@gmail.com>
2025-12-05 15:15:15 -0600

    Add AppUser entity

---
+++ EntryLog.Entities/POCOEntities/AppUser.cs
@@ -0,0 +1,17 @@
+﻿using EntryLog.Entities.Enums;
+
+namespace EntryLog.Entities.POCOEntities;
+
+public class AppUser
+{
+    public Guid Id { get; set; }
+    public int Code { get; set; }
+    public RoleType Role { get; set; }
+    public string Email { get; set; } = string.Empty;
+    public string CellPhone { get; set; } = string.Empty;
+    public string Password { get; set; } = string.Empty;
+    public int Attempts { get; set; }
+    public string? RecoveryToken { get; set; } = string.Empty;
+    public bool RecoveryTokenActive { get; set; }
+    public bool Active { get; set; }
+}
