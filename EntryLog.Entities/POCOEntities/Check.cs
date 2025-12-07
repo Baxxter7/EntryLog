027e176e8e23d8ad77513901c1bd852e2c34367b
Josue Martinez <josueisaiasmartinezlopez@gmail.com>
2025-12-07 15:45:14 -0600

    refactor: make DeviceName nullable

--- EntryLog.Entities/POCOEntities/Check.cs
+++ EntryLog.Entities/POCOEntities/Check.cs
@@ -3,7 +3,7 @@
 public class Check
 {
     public string Method { get; set; } = string.Empty;
-    public string DeviceName { get; set; } = string.Empty;
+    public string? DeviceName { get; set; } = string.Empty;
     public DateTime Date { get; set; }
     public Location Location { get; set; } = new();
     public string PhotoUrl { get; set; } = string.Empty;
