0f82b7eb7796c472cc51e9ed39b003a09f58a798
Josue Martinez <josueisaiasmartinezlopez@gmail.com>
2025-12-06 23:58:59 -0600

    feat: add EntryLogDbOptions

---
+++ EntryLog.Data/MongoDB/Config/EntryLogDbOptions.cs
@@ -0,0 +1,8 @@
+﻿namespace EntryLog.Data.MongoDB.Config
+{
+    internal sealed class EntryLogDbOptions
+    {
+        public string ConnectionUri { get; init; } = string.Empty;
+        public string DatabaseName { get; init; } = string.Empty;
+    }
+}
