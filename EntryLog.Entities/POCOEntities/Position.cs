81945372db0129bcb86dfbb261fbed978d28e650
Josue Martinez <josueisaiasmartinezlopez@gmail.com>
2025-12-08 22:53:59 -0600

    feat: add Position entity

---
+++ EntryLog.Entities/POCOEntities/Position.cs
@@ -0,0 +1,10 @@
+﻿namespace EntryLog.Entities.POCOEntities; 
+
+public sealed class Position
+{
+    public int Id { get; set; }
+    public string Name { get; set; } = string.Empty;
+    public string Description { get; set; } = string.Empty;
+    public ICollection<Employee> Employees { get; set; } = [];
+     
+}
