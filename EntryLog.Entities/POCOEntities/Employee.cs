4ae8e7f9e95466efe40568b859f1f33f70e63eb6
Josue Martinez <josueisaiasmartinezlopez@gmail.com>
2025-12-09 00:25:47 -0600

    refactor: Employee entity — updated PositionId and DateOfBirthday, and added Position property.

--- EntryLog.Entities/POCOEntities/Employee.cs
+++ EntryLog.Entities/POCOEntities/Employee.cs
@@ -4,9 +4,11 @@ public class Employee
 {
     public int Code { get; set; }
     public string FullName { get; set; } = string.Empty;
-    public string Position { get; set; } = string.Empty;
-    public int OrganizationID { get; set; }
+    public string PositionId { get; set; } = string.Empty;
+    public DateTime DateofBirthday { get; set; }
     public string BranchOffice { get; set; } = string.Empty;
     public string TownName { get; set; } = string.Empty;
     public string CostCenter { get; set; } = string.Empty;
+    public Position Position { get; set; } = new Position();
+
 }
