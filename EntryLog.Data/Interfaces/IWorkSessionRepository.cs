a035977001c646bcd0328e828f7f2d085d3ccf1f
Josue Martinez <josueisaiasmartinezlopez@gmail.com>
2025-12-09 23:11:20 -0600

    feat: add IWorkSessionRepository

---
+++ EntryLog.Data/Interfaces/IWorkSessionRepository.cs
@@ -0,0 +1,12 @@
+﻿using EntryLog.Entities.POCOEntities;
+
+namespace EntryLog.Data.Interfaces;
+
+public interface IWorkSessionRepository
+{
+    Task CreateAsync(WorkSession workSession);
+    Task UpdateAsync(WorkSession workSession);
+    Task<WorkSession?> GetByIdAsync(Guid id);
+    Task<WorkSession?> GetByEmpleadoAsync(int id);
+    Task<IEnumerable<WorkSession>> GetAllAsync();
+}
