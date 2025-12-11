c79f625e7eb8c05bd1249c6fba713deb4b65c708
Josue Martinez <josueisaiasmartinezlopez@gmail.com>
2025-12-10 22:35:32 -0600

    feat: Add WorkSession repository implementation

---
+++ EntryLog.Data/MongoDB/Repositories/WorkSessionRepository.cs
@@ -0,0 +1,35 @@
+﻿using EntryLog.Data.Constants;
+using EntryLog.Data.Interfaces;
+using EntryLog.Entities.POCOEntities;
+using MongoDB.Driver;
+
+namespace EntryLog.Data.MongoDB.Repositories;
+
+internal class WorkSessionRepository(IMongoDatabase database) : IWorkSessionRepository
+{
+    private readonly IMongoCollection<WorkSession> _collection = database.GetCollection<WorkSession>(CollectionNames.WorkSessions);
+    public async Task CreateAsync(WorkSession workSession)
+    {
+        await _collection.InsertOneAsync(workSession);
+    }
+
+    public Task<IEnumerable<WorkSession>> GetAllAsync()
+    {
+        throw new NotImplementedException();
+    }
+
+    public async Task<WorkSession?> GetByEmpleadoAsync(int id)
+    {
+        return await _collection.Find(x => x.EmployeeId  == id).FirstOrDefaultAsync();
+    }
+
+    public async Task<WorkSession?> GetByIdAsync(Guid id)
+    {
+        return await _collection.Find(x => x.Id == id).FirstOrDefaultAsync();
+    }
+
+    public async Task UpdateAsync(WorkSession workSession)
+    {
+        await _collection.ReplaceOneAsync(x => x.Id == workSession.Id,  workSession);
+    }
+}
