eeda85c9d8b0aed6e1b46459a28aa2a3dea7de07
Josue Martinez <josueisaiasmartinezlopez@gmail.com>
2025-12-10 23:08:37 -0600

    feat:SpecificatinMethods

---
+++ EntryLog.Data/Specifications/SpecificationMethods.cs
@@ -0,0 +1,18 @@
+﻿using System.Linq.Expressions;
+
+namespace EntryLog.Data.Specifications;
+
+internal static class SpecificationMethods<TEntity> where TEntity : class
+{
+    public static Expression<Func<TEntity, bool>> And(
+        Expression<Func<TEntity, bool>> from,
+        Expression<Func<TEntity, bool>> to)
+    {
+        return Expression.Lambda<Func<TEntity, bool>>(
+            Expression.AndAlso(
+                new RewriterVisitor(from.Parameters[0], to.Parameters[0])
+                    .Visit(from.Body),
+                to.Body),
+            to.Parameters);
+    }
+}
