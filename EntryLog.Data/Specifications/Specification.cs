e775e3dfa075d4dcd080908cf805e768cffae25e
Josue Martinez <josueisaiasmartinezlopez@gmail.com>
2025-12-10 23:08:53 -0600

    feat: add specification

---
+++ EntryLog.Data/Specifications/Specification.cs
@@ -0,0 +1,10 @@
+﻿using System.Linq.Expressions;
+
+namespace EntryLog.Data.Specifications;
+
+internal abstract class Specification<TEntity> where TEntity : class
+{
+    public abstract Expression<Func<TEntity, bool>> Expression { get; set; }
+
+    public abstract void AndAlso(Expression<Func<TEntity, bool>> expression);
+}
