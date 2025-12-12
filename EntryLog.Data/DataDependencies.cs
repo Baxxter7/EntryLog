45c4b29a023f5fa571a7f2cf91cd6136e1f178b0
Josue Martinez <josueisaiasmartinezlopez@gmail.com>
2025-12-11 23:55:40 -0600

    feat: add configuration dependency injection

---
+++ EntryLog.Data/DataDependencies.cs
@@ -0,0 +1,42 @@
+﻿using EntryLog.Data.Interfaces;
+using EntryLog.Data.MongoDB.Config;
+using EntryLog.Data.MongoDB.Repositories;
+using EntryLog.Data.SqlLegacy.Contexts;
+using EntryLog.Data.SqlLegacy.Repositories;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Options;
+using MongoDB.Driver;
+
+namespace EntryLog.Data;
+
+
+public static class IDataDependencies
+{
+    public static IServiceCollection AddDataServices(this IServiceCollection services,
+        IConfiguration configuration)
+    {
+
+        //SQL
+        services.AddDbContext<EmployeesDbContext>(options =>
+        options.UseSqlServer(configuration.GetConnectionString("EmployeeDB")));
+
+        services.AddScoped<IEmployeeRepository, EmployeeRepository>();
+
+        //Mongo 
+        services.Configure<EntryLogDbOptions>(configuration.GetSection(nameof(EntryLogDbOptions)));
+
+        services.AddScoped<IMongoDatabase>(sp =>
+        {
+            var options = sp.GetRequiredService<IOptions<EntryLogDbOptions>>().Value;
+            var client = new MongoClient(options.ConnectionUri);
+            return client.GetDatabase(options.DatabaseName);
+        });
+
+        services.AddScoped<IAppUserRepository, AppUserRepository>();
+        services.AddScoped<IWorkSessionRepository, WorkSessionRepository>();
+
+        return services;
+    }
+}
