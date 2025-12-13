90c36a9291a0f12ccb3469dce93a1061bda308b6
Josue Martinez <josueisaiasmartinezlopez@gmail.com>
2025-12-13 17:59:12 -0600

    feat: add workSessionDto

---
+++ EntryLog.Business/DTOs/WorkSessionParameters.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Http;
+
+namespace EntryLog.Business.DTOs; 
+
+public record WorkSessionParameters(
+   string UserId,
+   string Method,
+   string DeviceName,
+   string Latitude,
+   string IpAddress,
+   IFormFile Image,
+   string? Notes
+    );
