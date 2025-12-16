514e9b9af67a3c52d6ceccca5ac852179d02245c
Josue Martinez <josueisaiasmartinezlopez@gmail.com>
2025-12-15 23:58:24 -0600

    feat: add GetWorkSessionDto

---
+++ EntryLog.Business/DTOs/GetWorkSessionDto.cs
@@ -0,0 +1,26 @@
+﻿namespace EntryLog.Business.DTOs;
+
+public record GetWorkSessionDto
+(
+    string Id,
+    int EmployeeId,
+    GetCheckDto CheckIn,
+    GetCheckDto CheckOut,
+    TimeSpan? TotalWorked,
+    string Status
+);
+
+public record GetCheckDto(
+    string Method,
+    string DeviceName,
+    DateTime Date,
+    GetLocationDto Location,
+    string PhotoUrl,
+    string Notes
+);
+
+public record GetLocationDto(
+   string Latitude,
+   string Longitude,
+   string IpAddress
+ );
