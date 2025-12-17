4508d75a2bac834cca744026496c4ac38c8f3799
Josue Martinez <josueisaiasmartinezlopez@gmail.com>
2025-12-16 22:30:04 -0600

    feat: rename params

--- EntryLog.Business/Interfaces/IWorkSessionServices.cs
+++ EntryLog.Business/Interfaces/IWorkSessionServices.cs
@@ -1,9 +1,11 @@
 ﻿using EntryLog.Business.DTOs;
+using EntryLog.Business.QueryFilters;
 
 namespace EntryLog.Business.Interfaces;
 
 public interface IWorkSessionServices
 {
-    Task<(bool success, string message)> OpenJobSession(CreateJobSessionDTO sessionDTO);
-    Task<(bool success, string message)> ClosedJobSession(CloseJobSessionDTO sessionDTO);
+    Task<(bool success, string message)> OpenJobSession(CreateJobSessionDto sessionDto);
+    Task<(bool success, string message)> ClosedJobSession(CloseJobSessionDto sessionDto);
+    Task<IEnumerable<object>> GetSessionListByFilterAsync(WorkSessionQueryFilter filter);
 }
