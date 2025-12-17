9ecbfe9d825d9454a80f0fcf36ce6fea050b3dce
Josue Martinez <josueisaiasmartinezlopez@gmail.com>
2025-12-16 22:31:21 -0600

    feat: refactor WorkSessionService

--- EntryLog.Business/Services/WorkSessionServices.cs
+++ EntryLog.Business/Services/WorkSessionServices.cs
@@ -1,5 +1,7 @@
 ﻿using EntryLog.Business.DTOs;
 using EntryLog.Business.Interfaces;
+using EntryLog.Business.QueryFilters;
+using EntryLog.Business.Specs;
 using EntryLog.Data.Interfaces;
 using EntryLog.Entities.Enums;
 using EntryLog.Entities.POCOEntities;
@@ -22,9 +24,9 @@ public class WorkSessionServices : IWorkSessionServices
         _employeeRepository = employeeRepository;
     }
 
-    public async Task<(bool success, string message)> ClosedJobSession(CloseJobSessionDTO sessionDTO)
+    public async Task<(bool success, string message)> ClosedJobSession(CloseJobSessionDto sessionDto)
     {
-        int code = int.Parse(sessionDTO.UserId);
+        int code = int.Parse(sessionDto.UserId);
         Employee? employee = await _employeeRepository.GetByCodeAsync(code);
 
         if (employee == null)
@@ -41,24 +43,37 @@ public class WorkSessionServices : IWorkSessionServices
             return (false, "No existe sesion activa para el usuario");
 
         activeSession.CheckOut ??= new Check();
-        activeSession.CheckOut.Method = sessionDTO.Method;
-        activeSession.CheckOut.DeviceName = sessionDTO.DeviceName;
+        activeSession.CheckOut.Method = sessionDto.Method;
+        activeSession.CheckOut.DeviceName = sessionDto.DeviceName;
         activeSession.CheckOut.Date = DateTime.UtcNow;
-        activeSession.CheckOut.Location.Latitude = sessionDTO.Latitude;
-        activeSession.CheckOut.Location.Longitude = sessionDTO.Longitude;
-        activeSession.CheckOut.Location.IpAddress = sessionDTO.IpAddress;
+        activeSession.CheckOut.Location.Latitude = sessionDto.Latitude;
+        activeSession.CheckOut.Location.Longitude = sessionDto.Longitude;
+        activeSession.CheckOut.Location.IpAddress = sessionDto.IpAddress;
         activeSession.CheckOut.PhotoUrl = string.Empty;
-        activeSession.CheckOut.Notes = sessionDTO.Notes;
+        activeSession.CheckOut.Notes = sessionDto.Notes;
         activeSession.Status = SessionStatus.Completed;
 
         await _workSessionRepository.UpdateAsync(activeSession);
         return (true, "Se registro exitosamente la sesión");
     }
 
-    public async Task<(bool success, string message)> OpenJobSession(CreateJobSessionDTO sessionDTO)
+    public async Task<IEnumerable<object>> GetSessionListByFilterAsync(WorkSessionQueryFilter filter)
+    {
+        var spec = new WorkSessionSpec();
+
+        if (filter.EmployeeId.HasValue)
+            spec.AndAlso(x => x.EmployeeId == filter.EmployeeId.Value);
+
+        IEnumerable<WorkSession> sessions = await _workSessionRepository.GetAllAsync(spec);
+
+        //Hola
+        throw new NotImplementedException();
+    }
+
+    public async Task<(bool success, string message)> OpenJobSession(CreateJobSessionDto sessionDto)
     {
 
-        int code = int.Parse(sessionDTO.UserId);
+        int code = int.Parse(sessionDto.UserId);
         Employee? employee = await _employeeRepository.GetByCodeAsync(code);
 
         if (employee == null)
@@ -81,16 +96,16 @@ public class WorkSessionServices : IWorkSessionServices
             EmployeeId = user.Code,
             CheckIn = new Check
             {
-                Method = sessionDTO.Method,
-                DeviceName = sessionDTO.DeviceName,
+                Method = sessionDto.Method,
+                DeviceName = sessionDto.DeviceName,
                 Date = DateTime.UtcNow,
                 Location = new Location
                 {
-                    Latitude = sessionDTO.Latitude,
-                    Longitude = sessionDTO.Longitude,
-                    IpAddress = sessionDTO.IpAddress,
+                    Latitude = sessionDto.Latitude,
+                    Longitude = sessionDto.Longitude,
+                    IpAddress = sessionDto.IpAddress,
                 },
-                Notes = sessionDTO.Notes ?? null,
+                Notes = sessionDto.Notes ?? null,
                 PhotoUrl = string.Empty
             },
             Status = SessionStatus.InProgress
