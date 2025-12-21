3f0a84f2f7a52b985df887c7c3d3acf385dc72a9
Josue Martinez <josueisaiasmartinezlopez@gmail.com>
2025-12-21 15:01:33 -0600

    feat: add interface IAppUserService

---
+++ EntryLog.Business/Interfaces/IAppUserServices.cs
@@ -0,0 +1,8 @@
+﻿using EntryLog.Business.DTOs;
+
+namespace EntryLog.Business.Interfaces;
+
+public interface IAppUserServices
+{
+    Task<(bool sucess, string message)> RegisterEmployeeUserAsync(CreateEmployeeUserDto employeeDto);
+}
