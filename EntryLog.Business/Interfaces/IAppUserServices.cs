fefd980423fe7029f9f38bdccdbd2b9238ea31a3
Josue Martinez <josueisaiasmartinezlopez@gmail.com>
2025-12-23 00:04:41 -0600

    feat: IAppUserServices

--- EntryLog.Business/Interfaces/IAppUserServices.cs
+++ EntryLog.Business/Interfaces/IAppUserServices.cs
@@ -4,5 +4,8 @@ namespace EntryLog.Business.Interfaces;
 
 public interface IAppUserServices
 {
-    Task<(bool sucess, string message)> RegisterEmployeeUserAsync(CreateEmployeeUserDto employeeDto);
+    Task<(bool success, string message, LoginResponseDto? data)> RegisterEmployeeAsync(CreateEmployeeUserDto employeeDto);
+    Task<(bool success, string message, LoginResponseDto? data)>UserLoginAsync(UserCredentialsDto credentialsDto);
+    Task<(bool success, string message)> AccountRecoveryStartAsync(string username);
+    Task<(bool success, string message)> AccountRecoveryCompleteAsync(AccountRecoveryDto recoveryDto);
 }
