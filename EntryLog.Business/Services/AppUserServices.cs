3c04c95ca8e1ae7020bbd164af46cb63ea270df5
Josue Martinez <josueisaiasmartinezlopez@gmail.com>
2025-12-24 19:07:10 -0600

    feat: add AppUserServices

---
+++ EntryLog.Business/Services/AppUserServices.cs
@@ -0,0 +1,27 @@
+﻿using EntryLog.Business.DTOs;
+using EntryLog.Business.Interfaces;
+
+namespace EntryLog.Business.Services;
+
+internal class AppUserServices : IAppUserServices
+{
+    public Task<(bool success, string message)> AccountRecoveryCompleteAsync(AccountRecoveryDto recoveryDto)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<(bool success, string message)> AccountRecoveryStartAsync(string username)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<(bool success, string message, LoginResponseDto? data)> RegisterEmployeeAsync(CreateEmployeeUserDto employeeDto)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<(bool success, string message, LoginResponseDto? data)> UserLoginAsync(UserCredentialsDto credentialsDto)
+    {
+        throw new NotImplementedException();
+    }
+}
