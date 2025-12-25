cd1d4bfd1ac461e06e02ea729398b8e4995608c4
Josue Martinez <josueisaiasmartinezlopez@gmail.com>
2025-12-24 19:59:28 -0600

    feat: add implementation RegisterEmployeeAsync

--- EntryLog.Business/Services/AppUserServices.cs
+++ EntryLog.Business/Services/AppUserServices.cs
@@ -1,10 +1,23 @@
 ﻿using EntryLog.Business.DTOs;
 using EntryLog.Business.Interfaces;
+using EntryLog.Data.Interfaces;
+using EntryLog.Entities.Enums;
+using EntryLog.Entities.POCOEntities;
 
 namespace EntryLog.Business.Services;
 
 internal class AppUserServices : IAppUserServices
 {
+    private readonly IEmployeeRepository _employeeRepository;
+    private readonly IAppUserRepository _appUserRepository;
+    private readonly IPasswordHasherService _hasherService;
+
+    public AppUserServices(IEmployeeRepository employeeRepository, IAppUserRepository appUserRepository, IPasswordHasherService hasherService)
+    {
+        _employeeRepository = employeeRepository;
+        _appUserRepository = appUserRepository;
+        _hasherService = hasherService;
+    }
     public Task<(bool success, string message)> AccountRecoveryCompleteAsync(AccountRecoveryDto recoveryDto)
     {
         throw new NotImplementedException();
@@ -15,9 +28,45 @@ internal class AppUserServices : IAppUserServices
         throw new NotImplementedException();
     }
 
-    public Task<(bool success, string message, LoginResponseDto? data)> RegisterEmployeeAsync(CreateEmployeeUserDto employeeDto)
+    public async Task<(bool success, string message, LoginResponseDto? data)> RegisterEmployeeAsync(CreateEmployeeUserDto employeeDto)
     {
-        throw new NotImplementedException();
+        if(!int.TryParse(employeeDto.DocumentNumber, out int code))
+            return (false, "Número de documento inválido", null);
+
+        Employee? employee = await _employeeRepository.GetByCodeAsync(code);
+
+        if (employee == null)
+            return (false, "Empleado no encontrado", null);
+
+        AppUser? user = await _appUserRepository.GetByCodeAsync(code);
+
+        if (user != null)
+            return (false, "El empleado ya tiene un usuario registrado", null);
+
+        user = await _appUserRepository.GetByUserNameAsync(employeeDto.UserName);
+
+        if (user != null)
+            return (false, "El usuario ya existe", null);
+
+       if(string.IsNullOrEmpty(employeeDto.Password) || employeeDto.Password != employeeDto.PasswordConf) 
+            return (false, "El usuario ya existe", null);
+
+        user = new AppUser
+        {
+            Code = code,
+            Role = RoleType.Employee,
+            Email = employeeDto.UserName,
+            CellPhone = employeeDto.CellPhone,
+            //Password = employeeDto.Password,
+            Password = _hasherService.Hash(employeeDto.Password),
+            Attempts = 0,
+            RecoveryTokenActive = false,
+            Active = true
+        };
+
+        await _appUserRepository.CreateAsync(user);
+
+        return (true, "Login Successfull", new LoginResponseDto(user.Code, user.Role.ToString(), user.Email));
     }
 
     public Task<(bool success, string message, LoginResponseDto? data)> UserLoginAsync(UserCredentialsDto credentialsDto)
