43f3aef18660d4c159721bec9312277d498f2451
Josue Martinez <josueisaiasmartinezlopez@gmail.com>
2025-12-25 23:55:50 -0600

    refactor: change creation message

--- EntryLog.Business/Services/AppUserServices.cs
+++ EntryLog.Business/Services/AppUserServices.cs
@@ -65,7 +65,7 @@ internal class AppUserServices : IAppUserServices
 
         await _appUserRepository.CreateAsync(user);
 
-        return (true, "Employee created successfullys", new LoginResponseDto(user.Code, user.Role.ToString(), user.Email));
+        return (true, "Employee created successfully", new LoginResponseDto(user.Code, user.Role.ToString(), user.Email));
     }
 
     public async Task<(bool success, string message, LoginResponseDto? data)> UserLoginAsync(UserCredentialsDto credentialsDto)
