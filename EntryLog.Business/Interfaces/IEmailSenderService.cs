a4bb20e0d0b0f22d3f2f0c94af32eba79f3ab172
Josue Martinez <josueisaiasmartinezlopez@gmail.com>
2025-12-26 23:38:55 -0600

    refactor: make data parameter optional

--- EntryLog.Business/Interfaces/IEmailSenderService.cs
+++ EntryLog.Business/Interfaces/IEmailSenderService.cs
@@ -2,5 +2,5 @@
 internal interface IEmailSenderService
 {
     Task<bool> SendTextEmailAsync(string text, string to);
-    Task<bool> SendEmailWithTemplateAsync(string templateName, string to, object data);
+    Task<bool> SendEmailWithTemplateAsync(string templateName, string to, object? data = null);
 }
