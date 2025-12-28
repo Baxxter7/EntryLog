46005074c73d4939084c714404b25b2c39f3f423
Josue Martinez <josueisaiasmartinezlopez@gmail.com>
2025-12-28 15:41:39 -0600

    feat: add EncrytionKeyValues

---
+++ EntryLog.Business/Cryptography/EncryptionKeyValues.cs
@@ -0,0 +1,6 @@
+﻿namespace EntryLog.Business.Cryptography;
+internal class EncryptionKeyValues
+{
+    string PublicKey {  get; init; } = string.Empty;
+    string PrivateKey {  get; init; } = string.Empty;
+}
