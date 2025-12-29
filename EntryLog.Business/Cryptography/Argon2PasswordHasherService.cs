1d9e505e53fe383511be180f00685ef1dd5f359b
Josue Martinez <josueisaiasmartinezlopez@gmail.com>
2025-12-28 21:31:54 -0600

    feat: add Argon2PasswordHasherService

---
+++ EntryLog.Business/Cryptography/Argon2PasswordHasherService.cs
@@ -0,0 +1,22 @@
+﻿using EntryLog.Business.Interfaces;
+using Microsoft.Extensions.Options;
+
+namespace EntryLog.Business.Cryptography;
+
+internal class Argon2PasswordHasherService : IPasswordHasherService
+{
+    private readonly Argon2PasswordHashOptions _options;
+    public Argon2PasswordHasherService(IOptions<Argon2PasswordHashOptions> options)
+    {
+        _options = options.Value;
+    }
+    public string Hash(string password)
+    {
+        throw new NotImplementedException();
+    }
+
+    public bool Verify(string password, string hash)
+    {
+        throw new NotImplementedException();
+    }
+}
