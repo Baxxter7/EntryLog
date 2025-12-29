b4909c5de136952fe46047b206c49123e3976f4c
Josue Martinez <josueisaiasmartinezlopez@gmail.com>
2025-12-28 20:37:50 -0600

    refactor: change property visibility in EncryptionKeyValues.

--- EntryLog.Business/Cryptography/EncryptionKeyValues.cs
+++ EntryLog.Business/Cryptography/EncryptionKeyValues.cs
@@ -1,6 +1,6 @@
 ﻿namespace EntryLog.Business.Cryptography;
 internal class EncryptionKeyValues
 {
-    string PublicKey {  get; init; } = string.Empty;
-    string PrivateKey {  get; init; } = string.Empty;
+    public string PublicKey {  get; init; } = string.Empty;
+    public string PrivateKey {  get; init; } = string.Empty;
 }
--- EntryLog.Business/Cryptography/RsaAsymmetricService.cs
+++ EntryLog.Business/Cryptography/RsaAsymmetricService.cs
@@ -1,32 +1,60 @@
 ﻿using EntryLog.Business.Interfaces;
 using Microsoft.Extensions.Options;
+using System.Security.Cryptography;
+using System.Text;
 
 namespace EntryLog.Business.Cryptography;
 
 internal class RsaAsymmetricService : IEncryptionService
 {
-    private readonly EncryptionKeyValues _encryptionKeyValues;
+    private readonly EncryptionKeyValues _keys;
+    private readonly RSACryptoServiceProvider _csp = new RSACryptoServiceProvider((int)KeySize.SIZE_2048);
 
     public RsaAsymmetricService(IOptions<EncryptionKeyValues> options)
     {
-        _encryptionKeyValues = options.Value;
+        _keys = options.Value;
     }
 
     public string Encrypt(string plainText)
     {
-        throw new NotImplementedException();
+        try
+        {
+            _csp.FromXmlString(_keys.PublicKey);
+            byte[] data = Encoding.Unicode.GetBytes(plainText);
+            byte[] cipher = _csp.Encrypt(data, true);
+            return Convert.ToBase64String(cipher);
+        }
+        catch (Exception ex)
+        {
+            throw new CryptographicException("Encryption failed.", ex);
+        }
+        finally
+        {
+            _csp.PersistKeyInCsp = false;
+        }
     }
 
     public string Decrypt(string cipherText)
     {
-        throw new NotImplementedException();
+        try {
+            _csp.FromXmlString(_keys.PrivateKey);
+            byte[] data = Convert.FromBase64String(cipherText);
+            byte[] textPlain = _csp.Decrypt(data, true);
+            return Encoding.Unicode.GetString(textPlain);
+        }
+        catch (Exception ex)
+        {
+            throw new CryptographicException("Encryption failed.", ex);
+        }
+        finally
+        {
+            _csp.PersistKeyInCsp = false;
+        }
     }
 }
 public enum KeySize
 {
     SIZE_512 = 512,
     SIZE_1024 = 1024,
-    SIZE_2048 = 2048,
-    SIZE_952 = 952,
-    SIZE_1369 = 1369
+    SIZE_2048 = 2048
 }
