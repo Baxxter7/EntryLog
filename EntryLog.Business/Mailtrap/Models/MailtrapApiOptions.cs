32c2fbbb2e94deb524dc19c97dfa52f98f1efbce
Josue Martinez <josueisaiasmartinezlopez@gmail.com>
2025-12-29 23:50:04 -0600

    feat: add MailtrapApiOptions

---
+++ EntryLog.Business/Mailtrap/Models/MailtrapApiOptions.cs
@@ -0,0 +1,9 @@
+﻿namespace EntryLog.Business.Mailtrap.Models;
+
+internal class MailtrapApiOptions
+{
+    public string ApiUrl { get; init; } = string.Empty;
+    public string ApiToken{ get; init; } = string.Empty;
+    public string FromEmail { get; init; } = string.Empty;
+    public string FromName { get; init; } = string.Empty;
+}
