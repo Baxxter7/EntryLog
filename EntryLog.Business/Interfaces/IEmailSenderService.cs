142a5d160384ba9204f20a526110a28d8aa1beaa
Josue Martinez <josueisaiasmartinezlopez@gmail.com>
2025-12-30 23:43:13 -0600

    refactor: delete SendTextEmailAsync method

--- EntryLog.Business/Interfaces/IEmailSenderService.cs
+++ EntryLog.Business/Interfaces/IEmailSenderService.cs
@@ -1,6 +1,5 @@
 ﻿namespace EntryLog.Business.Interfaces;
 internal interface IEmailSenderService
 {
-    Task<bool> SendTextEmailAsync(string text, string to);
     Task<bool> SendEmailWithTemplateAsync(string templateName, string to, object? data = null);
 }
