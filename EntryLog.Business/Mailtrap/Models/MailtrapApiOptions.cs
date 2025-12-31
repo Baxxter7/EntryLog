045493358f008854d7715149a7739ed40cbe2bd4
Josue Martinez <josueisaiasmartinezlopez@gmail.com>
2025-12-30 23:42:39 -0600

    feat: add MailtrapApiOptions

--- EntryLog.Business/Mailtrap/Models/MailtrapApiOptions.cs
+++ EntryLog.Business/Mailtrap/Models/MailtrapApiOptions.cs
@@ -6,7 +6,7 @@ internal class MailtrapApiOptions
     public string ApiToken{ get; init; } = string.Empty;
     public string FromEmail { get; init; } = string.Empty;
     public string FromName { get; init; } = string.Empty;
-    public List<MailTrapTemplete> templates { get; init; } = new List<MailTrapTemplete>();
+    public List<MailTrapTemplete> Templates { get; init; } = new List<MailTrapTemplete>();
 }
 
 internal class MailTrapTemplete
