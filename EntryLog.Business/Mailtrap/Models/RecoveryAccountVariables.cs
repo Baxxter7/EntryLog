f797ded1337dc3fde9e05d77e522ca4695044114
Josue Martinez <josueisaiasmartinezlopez@gmail.com>
2025-12-30 23:44:53 -0600

    feat: add RecoveryAccountVariables

---
+++ EntryLog.Business/Mailtrap/Models/RecoveryAccountVariables.cs
@@ -0,0 +1,11 @@
+﻿using System.Text.Json.Serialization;
+
+namespace EntryLog.Business.Mailtrap.Models;
+internal class RecoveryAccountVariables
+{
+    [JsonPropertyName("name")]
+    public string Name { get; set; } = string.Empty;
+
+    [JsonPropertyName("url")]
+    public string Url { get; set; } = string.Empty;
+}
