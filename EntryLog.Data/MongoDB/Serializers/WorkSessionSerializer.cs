20b081d98b22a27398d99e021911c18e2e270afa
Josue Martinez <josueisaiasmartinezlopez@gmail.com>
2026-01-02 00:34:23 -0600

    feat: add setIdMember

--- EntryLog.Data/MongoDB/Serializers/WorkSessionSerializer.cs
+++ EntryLog.Data/MongoDB/Serializers/WorkSessionSerializer.cs
@@ -20,6 +20,8 @@ internal static class WorkSessionSerializer
                 .SetIdGenerator(GuidGenerator.Instance)
                 .SetSerializer(new GuidSerializer(GuidRepresentation.Standard));
 
+                cm.SetIdMember(cm.GetMemberMap(x => x.Id));
+
                 cm.MapMember(x => x.EmployeeId).SetElementName("id_empleado");
                 cm.MapMember(x => x.CheckIn).SetElementName("entrada");
 
