37175dfa1c0ef484f69b314ddbfd02e45f373c4e
Josue Martinez <josueisaiasmartinezlopez@gmail.com>
2026-01-01 21:10:03 -0600

    refactor: Correct className MailTrapTemplate

--- EntryLog.Business/Mailtrap/Models/MailtrapApiOptions.cs
+++ EntryLog.Business/Mailtrap/Models/MailtrapApiOptions.cs
@@ -6,10 +6,10 @@ internal class MailtrapApiOptions
     public string ApiToken{ get; init; } = string.Empty;
     public string FromEmail { get; init; } = string.Empty;
     public string FromName { get; init; } = string.Empty;
-    public List<MailTrapTemplete> Templates { get; init; } = new List<MailTrapTemplete>();
+    public List<MailTrapTemplate> Templates { get; init; } = new List<MailTrapTemplate>();
 }
 
-internal class MailTrapTemplete
+internal class MailTrapTemplate
 {
     public string Uuid { get; init; } = string.Empty;
     public string Name { get; init; } = string.Empty;
