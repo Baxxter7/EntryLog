a75178ad0c438f9430aa4e06689b5d260b35c042
Josue Martinez <josueisaiasmartinezlopez@gmail.com>
2026-01-02 20:26:39 -0600

    feat: add web API project

---
+++ EntryLog.Api/Controllers/TestController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace EntryLog.Api.Controllers;
+
+[Route("api/[controller]")]
+[ApiController]
+public class TestController : ControllerBase
+{
+    [HttpGet]
+    public ActionResult Get()
+    {
+        return Ok("Working");
+    }
+}
