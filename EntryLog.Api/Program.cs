24236376143b6ba446f27a28ded99395fda8353d
Josue Martinez <josueisaiasmartinezlopez@gmail.com>
2026-01-04 00:28:53 -0600

    feat: add services to program.cs

--- EntryLog.Api/Program.cs
+++ EntryLog.Api/Program.cs
@@ -1,6 +1,11 @@
+using EntryLog.Business;
+using EntryLog.Data;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+builder.Services.AddDataServices(builder.Configuration)
+                .AddBusinessServices(builder.Configuration);
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
