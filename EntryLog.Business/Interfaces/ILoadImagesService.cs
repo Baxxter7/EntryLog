beed6c7ce0472056fef383a41a773f7dc89c3baf
Josue Martinez <josueisaiasmartinezlopez@gmail.com>
2026-01-07 23:09:13 -0600

    feat: add ILoadImagesService

---
+++ EntryLog.Business/Interfaces/ILoadImagesService.cs
@@ -0,0 +1,8 @@
+﻿using EntryLog.Business.DTOs;
+
+namespace EntryLog.Business.Interfaces;
+
+internal interface ILoadImagesService
+{
+    Task<ImageBBResponseDto> UploadAsync(Stream image, string fileName, string extension);
+}
