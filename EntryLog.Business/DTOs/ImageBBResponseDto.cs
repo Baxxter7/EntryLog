5c4de3f86cd6845e6a5b440c840c5b8f05d199cd
Josue Martinez <josueisaiasmartinezlopez@gmail.com>
2026-01-07 23:08:42 -0600

    feat: add ImageBBResponseDto

---
+++ EntryLog.Business/DTOs/ImageBBResponseDto.cs
@@ -0,0 +1,114 @@
+﻿using System.Text.Json.Serialization;
+
+namespace EntryLog.Business.DTOs;
+
+internal class ImageBBResponseDto
+{
+    [JsonPropertyName("data")]
+    public Data Data { get; set; }
+
+    [JsonPropertyName("success")]
+    public bool Success { get; set; }
+
+    [JsonPropertyName("status")]
+    public int Status { get; set; }
+}
+
+internal class Data
+{
+    [JsonPropertyName("id")]
+    public string Id { get; set; }
+
+    [JsonPropertyName("title")]
+    public string Title { get; set; }
+
+    [JsonPropertyName("url_viewer")]
+    public string UrlViewer { get; set; }
+
+    [JsonPropertyName("url")]
+    public string Url { get; set; }
+
+    [JsonPropertyName("display_url")]
+    public string DisplayUrl { get; set; }
+
+    [JsonPropertyName("width")]
+    public int Width { get; set; }
+
+    [JsonPropertyName("height")]
+    public int Height { get; set; }
+
+    [JsonPropertyName("size")]
+    public int Size { get; set; }
+
+    [JsonPropertyName("time")]
+    public long Time { get; set; }
+
+    [JsonPropertyName("expiration")]
+    public int Expiration { get; set; }
+
+    [JsonPropertyName("image")]
+    public Image Image { get; set; }
+
+    [JsonPropertyName("thumb")]
+    public Thumb Thumb { get; set; }
+
+    [JsonPropertyName("medium")]
+    public Medium Medium { get; set; }
+
+    [JsonPropertyName("delete_url")]
+    public string DeleteUrl { get; set; }
+}
+
+internal class Image
+{
+    [JsonPropertyName("filename")]
+    public string Filename { get; set; }
+
+    [JsonPropertyName("name")]
+    public string Name { get; set; }
+
+    [JsonPropertyName("mime")]
+    public string Mime { get; set; }
+
+    [JsonPropertyName("extension")]
+    public string Extension { get; set; }
+
+    [JsonPropertyName("url")]
+    public string Url { get; set; }
+}
+
+internal class Medium
+{
+    [JsonPropertyName("filename")]
+    public string Filename { get; set; }
+
+    [JsonPropertyName("name")]
+    public string Name { get; set; }
+
+    [JsonPropertyName("mime")]
+    public string Mime { get; set; }
+
+    [JsonPropertyName("extension")]
+    public string Extension { get; set; }
+
+    [JsonPropertyName("url")]
+    public string Url { get; set; }
+}
+
+internal class Thumb
+{
+    [JsonPropertyName("filename")]
+    public string Filename { get; set; }
+
+    [JsonPropertyName("name")]
+    public string Name { get; set; }
+
+    [JsonPropertyName("mime")]
+    public string Mime { get; set; }
+
+    [JsonPropertyName("extension")]
+    public string Extension { get; set; }
+
+    [JsonPropertyName("url")]
+    public string Url { get; set; }
+}
