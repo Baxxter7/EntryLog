005bff58651a267af2726bc685e543c83f118bea
Josue Martinez <josueisaiasmartinezlopez@gmail.com>
2026-01-08 21:51:36 -0600

    feat: add WorkSessionService

---
+++ EntryLog.Business/ImageBB/ImageBBService.cs
@@ -0,0 +1,20 @@
+﻿using EntryLog.Business.DTOs;
+using EntryLog.Business.Interfaces;
+using Microsoft.Extensions.Options;
+
+namespace EntryLog.Business.ImageBB;
+
+internal class ImageBBService : ILoadImagesService
+{
+    private readonly IOptions<ImageBBOptions> _options;
+
+    public ImageBBService(IOptions<ImageBBOptions> options)
+    {
+        _options = options;
+    }
+
+    public Task<ImageBBResponseDto> UploadAsync(Stream image, string fileName, string extension)
+    {
+        throw new NotImplementedException();
+    }
+}
