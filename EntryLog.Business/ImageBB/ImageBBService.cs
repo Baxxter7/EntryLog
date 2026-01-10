b7ed400f3a3c31779c14ef03d91db924a214711d
Josue Martinez <josueisaiasmartinezlopez@gmail.com>
2026-01-10 15:51:09 -0600

    feat: add ImageBBService

--- EntryLog.Business/ImageBB/ImageBBService.cs
+++ EntryLog.Business/ImageBB/ImageBBService.cs
@@ -1,20 +1,63 @@
-﻿using EntryLog.Business.DTOs;
+﻿using EntryLog.Business.Constants;
+using EntryLog.Business.DTOs;
 using EntryLog.Business.Interfaces;
 using Microsoft.Extensions.Options;
+using System.Text.Json;
 
 namespace EntryLog.Business.ImageBB;
 
 internal class ImageBBService : ILoadImagesService
 {
-    private readonly IOptions<ImageBBOptions> _options;
+    private readonly ImageBBOptions _options;
+    private readonly IHttpClientFactory _httpClientFactory;
 
-    public ImageBBService(IOptions<ImageBBOptions> options)
+    public ImageBBService(IOptions<ImageBBOptions> options, IHttpClientFactory httpClientFactory)
     {
-        _options = options;
+        _options = options.Value;
+        _httpClientFactory = httpClientFactory;
     }
 
-    public Task<ImageBBResponseDto> UploadAsync(Stream image, string fileName, string extension)
+    public async Task<ImageBBResponseDto?> UploadAsync(
+    Stream image,
+    string contentType,
+    string fileName,
+    string extension)
     {
-        throw new NotImplementedException();
+        using HttpClient client = _httpClientFactory.CreateClient(ApiNames.ImageBB);
+
+        using var content = CreateMultipartContent(image, contentType, fileName);
+
+        HttpResponseMessage response = await client.PostAsync(
+            BuildUploadUrl(),
+            content
+        );
+
+        if (!response.IsSuccessStatusCode)
+            return null;
+
+        string responseBody = await response.Content.ReadAsStringAsync();
+
+        return JsonSerializer.Deserialize<ImageBBResponseDto>(responseBody);
     }
+
+    private MultipartFormDataContent CreateMultipartContent(
+        Stream image,
+        string contentType,
+        string fileName)
+    {
+        var streamContent = new StreamContent(image);
+        streamContent.Headers.ContentType =
+            new System.Net.Http.Headers.MediaTypeHeaderValue(contentType);
+
+        var form = new MultipartFormDataContent();
+        form.Add(streamContent, "image", fileName);
+
+        return form;
+    }
+
+    private string BuildUploadUrl()
+    {
+        return $"/1/upload?expiration={_options.ExpirationSeconds}&key={_options.ApiToken}";
+    }
+
 }
