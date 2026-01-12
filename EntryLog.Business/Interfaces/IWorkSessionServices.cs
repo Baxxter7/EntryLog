0359b614c7fd89e31a178771b82b8fe78cae6d5e
Josue Martinez <josueisaiasmartinezlopez@gmail.com>
2026-01-11 23:39:07 -0600

    feat: add ImageTestAsync to Interface

--- EntryLog.Business/Interfaces/IWorkSessionServices.cs
+++ EntryLog.Business/Interfaces/IWorkSessionServices.cs
@@ -1,5 +1,6 @@
 ﻿using EntryLog.Business.DTOs;
 using EntryLog.Business.QueryFilters;
+using Microsoft.AspNetCore.Http;
 
 namespace EntryLog.Business.Interfaces;
 
@@ -8,4 +9,7 @@ public interface IWorkSessionServices
     Task<(bool success, string message)> OpenJobSession(CreateJobSessionDto sessionDto);
     Task<(bool success, string message)> ClosedJobSession(CloseJobSessionDto sessionDto);
     Task<IEnumerable<GetWorkSessionDto>> GetSessionListByFilterAsync(WorkSessionQueryFilter filter);
+
+    //TODO: ELIMINAR DESPUES
+    Task<(bool success, string message)> ImageTestAsync(IFormFile image);
 }
