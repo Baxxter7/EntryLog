b29d39ae2a7f2e8d07cf75c4b44c015ed5ef2f03
Josue Martinez <josueisaiasmartinezlopez@gmail.com>
2026-01-12 19:27:26 -0600

    refactor: rename method to include Async suffix

--- EntryLog.Business/Interfaces/IWorkSessionServices.cs
+++ EntryLog.Business/Interfaces/IWorkSessionServices.cs
@@ -1,15 +1,11 @@
 ﻿using EntryLog.Business.DTOs;
 using EntryLog.Business.QueryFilters;
-using Microsoft.AspNetCore.Http;
 
 namespace EntryLog.Business.Interfaces;
 
 public interface IWorkSessionServices
 {
-    Task<(bool success, string message)> OpenJobSession(CreateJobSessionDto sessionDto);
-    Task<(bool success, string message)> ClosedJobSession(CloseJobSessionDto sessionDto);
+    Task<(bool success, string message)> OpenJobSessionAsync(CreateJobSessionDto sessionDto);
+    Task<(bool success, string message)> ClosedJobSessionAsync(CloseJobSessionDto sessionDto);
     Task<IEnumerable<GetWorkSessionDto>> GetSessionListByFilterAsync(WorkSessionQueryFilter filter);
-
-    //TODO: ELIMINAR DESPUES
-    Task<(bool success, string message)> ImageTestAsync(IFormFile image);
 }
