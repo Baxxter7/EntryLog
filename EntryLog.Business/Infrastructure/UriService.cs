690b03d52569ddc70c4304b034f112e02c05cc4f
Josue Martinez <josueisaiasmartinezlopez@gmail.com>
2026-01-14 23:10:39 -0600

    feat: IUriService and UriService

---
+++ EntryLog.Business/Infrastructure/UriService.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Http;
+
+namespace EntryLog.Business.Infrastructure;
+
+internal class UriService
+{
+    private readonly IHttpContextAccessor _httpContext;
+
+    public UriService(IHttpContextAccessor httpContext)
+    {
+        _httpContext = httpContext;
+    }
+}
---
+++ EntryLog.Business/Interfaces/IUriService.cs
@@ -0,0 +1,9 @@
+﻿namespace EntryLog.Business.Interfaces;
+
+internal interface IUriService
+{
+    string ApplicationURL { get; }
+    string UserAgent { get; }
+    string Platform { get; }
+    string RemoteIpAddress { get; }
+}
