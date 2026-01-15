a74b3cc425587dc5b619570c97e8c2b22ba682fd
Josue Martinez <josueisaiasmartinezlopez@gmail.com>
2026-01-14 23:09:53 -0600

    feat: add jobSessionController

---
+++ EntryLog.Api/Controllers/JobSessionController.cs
@@ -0,0 +1,44 @@
+﻿using EntryLog.Business.DTOs;
+using EntryLog.Business.Interfaces;
+using EntryLog.Business.QueryFilters;
+using Microsoft.AspNetCore.Mvc;
+
+namespace EntryLog.Api.Controllers;
+
+[Route("api/[controller]")]
+[ApiController]
+public class JobSessionController : ControllerBase
+{
+    private readonly IWorkSessionServices _workSessionServices;
+
+    public JobSessionController(IWorkSessionServices workSessionServices)
+    {
+        _workSessionServices = workSessionServices;
+    }
+
+    [HttpPost("open")]
+    public async Task<object> OpenJobSessionAsync([FromBody] CreateJobSessionDto jobSessionDto)
+    {
+        (bool success, string message) = await _workSessionServices.OpenJobSessionAsync(jobSessionDto);
+        return Ok(new
+        {
+            success,
+            message
+        });
+    }
+
+    [HttpPost("close")]
+    public async Task<object> OpenJobSessionAsync([FromBody] CloseJobSessionDto jobSessionDto)
+    {
+        (bool success, string message) = await _workSessionServices.ClosedJobSessionAsync(jobSessionDto);
+        return Ok(new
+        {
+            success,
+            message
+        });
+    }
+
+    [HttpPost("filter")]
+    public async Task<IEnumerable<GetWorkSessionDto>> FilterAsync([FromQuery] WorkSessionQueryFilter filter)
+        => await _workSessionServices.GetSessionListByFilterAsync(filter);
+}
