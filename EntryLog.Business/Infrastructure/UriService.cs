2932078bd714ae580b5c3037afde940bee5724fb
Josue Martinez <josueisaiasmartinezlopez@gmail.com>
2026-01-15 23:27:32 -0600

    fix: HttpContext null check en UriService

--- EntryLog.Business/Infrastructure/UriService.cs
+++ EntryLog.Business/Infrastructure/UriService.cs
@@ -9,5 +9,8 @@ internal class UriService
     public UriService(IHttpContextAccessor httpContext)
     {
         _httpContext = httpContext;
+
+        var request = (_httpContext.HttpContext?.Request) ?? 
+            throw new Exception("Ha ocurrido un error al generar el contexto http de la aplicacion");
     }
 }
