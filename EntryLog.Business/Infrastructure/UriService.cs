d428e225ec48e93307d9dc72112c0e9c7118b22f
Josue Martinez <josueisaiasmartinezlopez@gmail.com>
2026-01-16 22:32:23 -0600

    feat: add UriService implementation

--- EntryLog.Business/Infrastructure/UriService.cs
+++ EntryLog.Business/Infrastructure/UriService.cs
@@ -1,8 +1,9 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using EntryLog.Business.Interfaces;
+using Microsoft.AspNetCore.Http;
 
 namespace EntryLog.Business.Infrastructure;
 
-internal class UriService
+internal class UriService : IUriService
 {
     private readonly IHttpContextAccessor _httpContext;
 
@@ -10,7 +11,18 @@ internal class UriService
     {
         _httpContext = httpContext;
 
-        var request = (_httpContext.HttpContext?.Request) ?? 
+        var request = (_httpContext.HttpContext?.Request) ??
             throw new InvalidOperationException("Ha ocurrido un error al generar el contexto http de la aplicacion");
+
+        ApplicationURL = $"{request.Scheme}://{request.Host}{request.PathBase}";
+        UserAgent = $"{request.Headers["Sec-Ch-Ua"]}";
+        Platform = $"{request.Headers["Sec-Ch-Ua-Platform"]}";
+        RemoteIpAddress = request.HttpContext.Connection.RemoteIpAddress?.ToString() ?? "Unknown RemoteIpAddress";
+
     }
+
+    public string ApplicationURL { get; private set; }
+    public string UserAgent { get; private set; }
+    public string Platform { get; private set; }
+    public string RemoteIpAddress { get; private set; }
 }
