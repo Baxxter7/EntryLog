cdd2976a086ec1857589920b1b0ff442ff351108
Josue Martinez <josueisaiasmartinezlopez@gmail.com>
2026-01-16 22:30:39 -0600

    feat: delete properties to CreateJobSessionDto

--- EntryLog.Business/DTOs/CreateJobSessionDTO.cs
+++ EntryLog.Business/DTOs/CreateJobSessionDTO.cs
@@ -4,11 +4,8 @@ namespace EntryLog.Business.DTOs;
 
 public record CreateJobSessionDto(
    string UserId,
-   string Method,
-   string DeviceName,
    string Latitude,
    string Longitude,
-   string IpAddress,
    IFormFile Image,
    string? Notes
 );
