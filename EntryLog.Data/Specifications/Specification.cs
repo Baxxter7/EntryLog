e357f3baeb4dc8b3fe6bc75762559d708b65f3f0
Josue Martinez <josueisaiasmartinezlopez@gmail.com>
2026-01-26 22:42:32 -0600

    refactor: mejorando la specification

--- EntryLog.Data/Specifications/Specification.cs
+++ EntryLog.Data/Specifications/Specification.cs
@@ -2,9 +2,42 @@
 
 namespace EntryLog.Data.Specifications;
 
-public abstract class Specification<TEntity> where TEntity : class
+public abstract class Specification<TEntity> : ISpecification<TEntity> where TEntity : class
 {
-    public abstract Expression<Func<TEntity, bool>> Expression { get; set; }
+    public Expression<Func<TEntity, bool>> Expression { get; private set; } = _ => true;
+    public Expression<Func<TEntity, object>>? OrderBy { get; private set; }
+    public Expression<Func<TEntity, object>>? OrderByDescending { get; private set; }
+    public int Take { get; private set; }
+    public int Skip { get; private set; }
+    public bool IsPagingEnabled { get; private set; }
 
-    public abstract void AndAlso(Expression<Func<TEntity, bool>> expression);
+    public void AndAlso(Expression<Func<TEntity, bool>> expression)
+    {
+        Expression = SpecificationMethods<TEntity>.And(expression, Expression);
+    }
+
+    public void AndOrderBy(Expression<Func<TEntity, object>> orderByExpression)
+    {
+        OrderBy = orderByExpression;
+        OrderByDescending = null;
+    }
+
+    public void AndOrderByDescending(Expression<Func<TEntity, object>> orderByDescendingExpression)
+    {
+        OrderByDescending = orderByDescendingExpression;
+        OrderBy = null;
+    }
+
+    public void ApplyPaging(int take, int skip)
+    {
+        if (take < 0)
+            throw new ArgumentOutOfRangeException(nameof(take));
+
+        if (skip < 0)
+            throw new ArgumentOutOfRangeException(nameof(skip));
+
+        Take = take;
+        Skip = skip;
+        IsPagingEnabled = true;
+    }
 }
