bbcdbb61ea7a04c3ec209ca9b852984fb304c086
Josue Martinez <josueisaiasmartinezlopez@gmail.com>
2026-01-29 22:34:49 -0600

    feat: add PaginationQuery

---
+++ EntryLog.Business/QueryFilters/PaginationQuery.cs
@@ -0,0 +1,10 @@
+﻿using EntryLog.Business.Enums;
+
+namespace EntryLog.Business.QueryFilters;
+
+public class PaginationQuery
+{
+    public SortType? Sort { get; set; }
+    public int? PageIndex { get; set; }
+    public int? PageSize { get; set; }
+}
