b8a57b57fb42a6ece29c0b0a51ebe8390e7f866e
Josue Martinez <josueisaiasmartinezlopez@gmail.com>
2026-01-30 23:33:45 -0600

    refactor: clean WorkSessionSpec

--- EntryLog.Business/Specs/WorkSessionSpec.cs
+++ EntryLog.Business/Specs/WorkSessionSpec.cs
@@ -5,11 +5,4 @@ using System.Linq.Expressions;
 namespace EntryLog.Business.Specs;
 
 internal class WorkSessionSpec : Specification<WorkSession>
-{
-    public override Expression<Func<WorkSession, bool>> Expression { get; set; } = _ => true;
-
-    public override void AndAlso(Expression<Func<WorkSession, bool>> expression)
-    {
-        Expression = SpecificationMethods<WorkSession>.And(expression, Expression);
-    }
-}
+{ }
