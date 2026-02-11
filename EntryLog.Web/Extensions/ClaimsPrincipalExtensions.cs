2a5d5abeed7a55b7501c6905859882ea980b491f
Josue Martinez <josueisaiasmartinezlopez@gmail.com>
2026-02-10 22:53:22 -0600

    feat: add GetUserData extension

---
+++ EntryLog.Web/Extensions/ClaimsPrincipalExtensions.cs
@@ -0,0 +1,23 @@
+﻿using EntryLog.Web.Models;
+using System.Security.Claims;
+
+namespace EntryLog.Web.Extensions;
+
+public static class ClaimsPrincipalExtensions
+{
+    public static UserViewModel? GetUserData(this ClaimsPrincipal claimsPrincipal)
+    {
+
+        if (claimsPrincipal?.Identity?.IsAuthenticated != true)
+            return null;
+
+        IEnumerable<Claim> claims = claimsPrincipal.Claims;
+
+        return new UserViewModel(
+            int.Parse(claims.First(x => x.Type == ClaimTypes.NameIdentifier).Value),
+            claims.First(x => x.Type == ClaimTypes.Email).Value,
+            claims.First(x => x.Type == ClaimTypes.Role).Value,
+            claims.First(x => x.Type == ClaimTypes.Name).Value
+         );
+    }
+}
