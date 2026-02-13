3f86aa178d61ae54bb459916570d59947f8616e5
Josue Martinez <josueisaiasmartinezlopez@gmail.com>
2026-02-12 22:28:44 -0600

    feat: add FaceID to entities

---
+++ EntryLog.Entities/POCOEntities/FaceID.cs
@@ -0,0 +1,9 @@
+﻿namespace EntryLog.Entities.POCOEntities;
+
+public class FaceID
+{
+    public string ImageUrl { get; set; } = string.Empty;
+    public DateTime RegisterDate {  get; set; }
+    public List<float> Descriptor { get; set; } = [];
+    public bool Active { get; set; }
+}
