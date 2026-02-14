1eb1d4b1cc1cc01619e68e346322f82e74de5e3d
Josue Martinez <josueisaiasmartinezlopez@gmail.com>
2026-02-14 09:05:35 -0600

    feat: add JwtValidator

---
+++ EntryLog.Business/JWT/JwtValidator.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.IdentityModel.Tokens;
+
+namespace EntryLog.Business.JWT;
+
+internal static class JwtValidator
+{
+    public static bool CustomLifeTimeValidator(DateTime? notBefore,
+        DateTime? expires,
+        SecurityToken securityToken,
+        TokenValidationParameters validationParameters)
+    {
+        var now = DateTime.UtcNow;
+
+        if (expires != null && expires <= now)
+            return false;
+
+        if(notBefore != null && notBefore > now)
+            return false;
+
+        return true;
+    }
+}
