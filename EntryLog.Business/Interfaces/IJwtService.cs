ea457effc77c986059f86d6b3db0bd8be64c1c5b
Josue Martinez <josueisaiasmartinezlopez@gmail.com>
2026-02-14 09:05:12 -0600

    feat: add IJwtService

---
+++ EntryLog.Business/Interfaces/IJwtService.cs
@@ -0,0 +1,7 @@
+﻿namespace EntryLog.Business.Interfaces;
+
+internal interface IJwtService
+{
+    Task<string> GenerateTokenAsync(string userId, string purpose, TimeSpan expiresIn);
+    IDictionary<string, string> ValidateToken(string token);
+}
