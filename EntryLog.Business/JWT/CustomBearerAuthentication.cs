9af826dd94f3a3fcc9512a439f2c47e39ade3fef
Josue Martinez <josueisaiasmartinezlopez@gmail.com>
2026-02-15 21:53:23 -0600

    feat: add GenerateTokenAsync method

---
+++ EntryLog.Business/JWT/CustomBearerAuthentication.cs
@@ -0,0 +1,43 @@
+﻿using EntryLog.Business.Interfaces;
+using Microsoft.Extensions.Options;
+using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+
+namespace EntryLog.Business.JWT;
+
+internal class CustomBearerAuthentication : IJwtService
+{
+    private readonly JwtConfiguration _jwtConfiguration;
+
+    public CustomBearerAuthentication(IOptions<JwtConfiguration> options)
+    {
+        _jwtConfiguration = options.Value;
+    }
+
+    public Task<string> GenerateTokenAsync(string userId, string purpose, TimeSpan expiresIn)
+    {
+        var securityKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_jwtConfiguration.Secret));
+        var credentials = new SigningCredentials(securityKey, SecurityAlgorithms.HmacSha256);
+
+        var claims = new List<Claim>
+        {
+            new(JwtRegisteredClaimNames.Sub, userId),
+            new("purpose", purpose),
+            new(JwtRegisteredClaimNames.Iat, DateTimeOffset.UtcNow.ToUnixTimeSeconds().ToString())
+        };
+
+        var token = new JwtSecurityToken(
+            claims: claims,
+            expires: DateTime.UtcNow.Add(expiresIn),
+            signingCredentials: credentials);
+
+        return Task.FromResult(new JwtSecurityTokenHandler().WriteToken(token));
+    }
+
+    public IDictionary<string, string> ValidateToken(string token)
+    {
+        throw new NotImplementedException();
+    }
+}
