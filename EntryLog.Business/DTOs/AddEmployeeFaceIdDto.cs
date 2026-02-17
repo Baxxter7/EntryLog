6e5b6f7e90d8a401bfe945f9cf26cbadcbb0b8a1
Josue Martinez <josueisaiasmartinezlopez@gmail.com>
2026-02-16 22:49:17 -0600

    feat: add AddEmployeeFaceIdDto

---
+++ EntryLog.Business/DTOs/AddEmployeeFaceIdDto.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.Http;
+
+namespace EntryLog.Business.DTOs;
+
+public record AddEmployeeFaceIdDto
+(
+    int EmployeeCode,
+    IFormFile image,
+    List<float> Descriptor
+);
