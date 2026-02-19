94985e13edb88dc397985011fd8bed3e452093d3
Josue Martinez <josueisaiasmartinezlopez@gmail.com>
2026-02-19 00:02:25 -0600

    feat: add MapToEmployeeFaceIdDto

---
+++ EntryLog.Business/Mappers/FaceIdMapper.cs
@@ -0,0 +1,16 @@
+﻿using EntryLog.Business.DTOs;
+using EntryLog.Business.Utils;
+using EntryLog.Entities.POCOEntities;
+using System.Globalization;
+
+namespace EntryLog.Business.Mappers;
+
+internal class FaceIdMapper
+{
+    public static EmployeeFaceIdDto MapToEmployeeFaceIdDto(FaceID faceId, String base64Image)
+    => new(base64Image,
+        TimeFunctions.GetCentralAmericaStandardTime(faceId.RegisterDate)
+            .ToString("dd/MM/yyyy hh:mm tt", CultureInfo.InvariantCulture),
+        faceId.Active
+       );
+}
