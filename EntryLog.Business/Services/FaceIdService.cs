7432dab8378c1a3c6fc703ef82aeb64a7012b9fc
Josue Martinez <josueisaiasmartinezlopez@gmail.com>
2026-02-19 16:17:12 -0600

    feat: add FaceIdService

---
+++ EntryLog.Business/Services/FaceIdService.cs
@@ -0,0 +1,47 @@
+﻿using EntryLog.Business.DTOs;
+using EntryLog.Business.Interfaces;
+using EntryLog.Data.Interfaces;
+
+namespace EntryLog.Business.Services;
+
+internal class FaceIdService : IFaceIdService
+{
+    private readonly IAppUserRepository _userRepository;
+    private readonly IEmployeeRepository _employeeRepository;
+    private readonly ILoadImagesService _loadImagesService;
+    private readonly IHttpClientFactory _httpClientFactory;
+    private readonly IJwtService _jwtService;
+
+    public FaceIdService(IAppUserRepository userRepository, 
+        IEmployeeRepository employeeRepository, 
+        ILoadImagesService loadImagesService,
+        IHttpClientFactory httpClientFactory, 
+        IJwtService jwtService)
+    {
+        _userRepository = userRepository;
+        _employeeRepository = employeeRepository;
+        _loadImagesService = loadImagesService;
+        _httpClientFactory = httpClientFactory;
+        _jwtService = jwtService;
+    }
+
+    public Task<(bool success, string message, EmployeeFaceIdDto? data)> CreateEmployeeFaceIdAsync(AddEmployeeFaceIdDto faceIdDto)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<string> GenerateReferenceImageTokenAsync(string userId)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<EmployeeFaceIdDto> GetFaceIdAsync(int employeeCode)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<string> GetReferenceImageAsync(string authHeader)
+    {
+        throw new NotImplementedException();
+    }
+}
