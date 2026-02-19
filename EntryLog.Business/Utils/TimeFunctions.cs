003b1092a6cdf10dd37450563cd2bc6ccb6c8f67
Josue Martinez <josueisaiasmartinezlopez@gmail.com>
2026-02-19 00:02:03 -0600

    feat: implenment TimeFunctions

--- EntryLog.Business/Utils/TimeFunctions.cs
+++ EntryLog.Business/Utils/TimeFunctions.cs
@@ -2,4 +2,11 @@
 
 public static class TimeFunctions
 {
+    private const string CENTRAL_AMERICA_STANDARD_TIME = "Central America Standard Time";
+
+    public static DateTime GetCentralAmericaStandardTime()
+        => TimeZoneInfo.ConvertTimeFromUtc(DateTime.UtcNow, TimeZoneInfo.FindSystemTimeZoneById(CENTRAL_AMERICA_STANDARD_TIME));
+
+    public static DateTime GetCentralAmericaStandardTime(DateTime time)
+        => TimeZoneInfo.ConvertTimeFromUtc(time, TimeZoneInfo.FindSystemTimeZoneById(CENTRAL_AMERICA_STANDARD_TIME));
 }
