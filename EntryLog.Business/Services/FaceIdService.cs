4c3d9cebbaa41a6961e3cb748235c744644ddc73
Josue Martinez <josueisaiasmartinezlopez@gmail.com>
2026-02-20 23:47:04 -0600

    feat: implement CreateEmployeeFaceIdAsync

--- EntryLog.Business/Services/FaceIdService.cs
+++ EntryLog.Business/Services/FaceIdService.cs
@@ -1,6 +1,8 @@
 ﻿using EntryLog.Business.DTOs;
 using EntryLog.Business.Interfaces;
 using EntryLog.Data.Interfaces;
+using EntryLog.Entities.POCOEntities;
+using System.Text.Json;
 
 namespace EntryLog.Business.Services;
 
@@ -11,6 +13,8 @@ internal class FaceIdService : IFaceIdService
     private readonly ILoadImagesService _loadImagesService;
     private readonly IHttpClientFactory _httpClientFactory;
     private readonly IJwtService _jwtService;
+
+    private const int DescriptorLength = 128;
 
     public FaceIdService(IAppUserRepository userRepository, 
         IEmployeeRepository employeeRepository, 
@@ -25,9 +29,43 @@ internal class FaceIdService : IFaceIdService
         _jwtService = jwtService;
     }
 
-    public Task<(bool success, string message, EmployeeFaceIdDto? data)> CreateEmployeeFaceIdAsync(AddEmployeeFaceIdDto faceIdDto)
+    public async Task<(bool success, string message, EmployeeFaceIdDto? data)> CreateEmployeeFaceIdAsync(AddEmployeeFaceIdDto faceIdDto)
     {
-        throw new NotImplementedException();
+        if (faceIdDto.image is null || faceIdDto.image.Length == 0)
+            return (false, "Image is required", null);
+
+        if (string.IsNullOrWhiteSpace(faceIdDto.Descriptor))
+            return (false, "Descriptor is required", null);
+
+        List<float> descriptor = new List<float>();
+        try
+        {
+            descriptor = JsonSerializer.Deserialize<List<float>>(faceIdDto.Descriptor);
+        }
+        catch (JsonException ex) 
+        {
+            return (false, $"Descriptor format error: {ex.Message}", null);
+        }
+
+        if (descriptor is null || descriptor.Count != DescriptorLength)
+            return (false, "Descriptor lenght non validad", null);
+
+        var userTask = _userRepository.GetByCodeAsync(faceIdDto.EmployeeCode);
+        var employeeTask = _employeeRepository.GetByCodeAsync(faceIdDto.EmployeeCode);
+
+        await Task.WhenAll(userTask,  employeeTask);
+        AppUser user = await userTask;
+        Employee employee = await employeeTask;
+
+        if (employee is null)
+            return (false,"ha ocurrido un problema al obtener el employee", null);
+
+        if(user is null)
+            return (false,"ha ocurrido un problema al obtener el user", null);
+
+        if (user.FaceID is not null && user.FaceID.Active)
+            return (false, "FaceID ya configurado", null);
+
     }
 
     public Task<string> GenerateReferenceImageTokenAsync(string userId)
