0ad64daafda6e19d2c4c2093a663a546a1152ae0
Josue Martinez <josueisaiasmartinezlopez@gmail.com>
2026-02-22 20:12:46 -0600

    refactor: change uploadAsync return value

--- EntryLog.Business/Interfaces/ILoadImagesService.cs
+++ EntryLog.Business/Interfaces/ILoadImagesService.cs
@@ -5,5 +5,5 @@ namespace EntryLog.Business.Interfaces;
 internal interface ILoadImagesService
 {
     //Task<ImageBBResponseDto> UploadAsync(Stream image, string fileName, string extension);
-    Task<ImageBBResponseDto?> UploadAsync(Stream image, string type, string fileName, string extension);
+    Task<string?> UploadAsync(Stream image, string type, string fileName);
 }
