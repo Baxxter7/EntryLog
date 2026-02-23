369b686af9d0b0989d4eca8d7241d4f6ccdbebe7
Josue Martinez <josueisaiasmartinezlopez@gmail.com>
2026-02-22 20:14:47 -0600

    refactor: remove unused reference

--- EntryLog.Business/Interfaces/ILoadImagesService.cs
+++ EntryLog.Business/Interfaces/ILoadImagesService.cs
@@ -1,9 +1,6 @@
-﻿using EntryLog.Business.DTOs;
-
-namespace EntryLog.Business.Interfaces;
+﻿namespace EntryLog.Business.Interfaces;
 
 internal interface ILoadImagesService
 {
-    //Task<ImageBBResponseDto> UploadAsync(Stream image, string fileName, string extension);
     Task<string?> UploadAsync(Stream image, string type, string fileName);
 }
