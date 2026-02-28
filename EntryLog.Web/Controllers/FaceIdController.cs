4b098b74c0855d6821d621a09ea9f35c908ab454
Josue Martinez <josueisaiasmartinezlopez@gmail.com>
2026-02-27 23:50:21 -0600

    feat: add FaceIdController

---
+++ EntryLog.Web/Controllers/FaceIdController.cs
@@ -0,0 +1,23 @@
+﻿using EntryLog.Business.Interfaces;
+using EntryLog.Web.Extensions;
+using EntryLog.Web.Models;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+namespace EntryLog.Web.Controllers;
+
+[Authorize(Roles = "Employee")]
+public class FaceIdController : Controller
+{
+    private readonly IFaceIdService _faceIdService;
+
+    public FaceIdController(IFaceIdService faceIdService)
+    {
+        _faceIdService = faceIdService;
+    }
+
+    public async Task<IActionResult> Index()
+    {
+        UserViewModel userData = User.GetUserData()!;
+        return View(await _faceIdService.GetFaceIdAsync(userData.NameIdentifier));
+    }
+}
