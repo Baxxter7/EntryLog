f92aa11b8faf36e0a5bc279425e6256ee6ed0696
Josue Martinez <josueisaiasmartinezlopez@gmail.com>
2026-03-05 23:43:24 -0600

    feat: add descriptor to createWorkSession

--- EntryLog.Business/Interfaces/IWorkSessionServices.cs
+++ EntryLog.Business/Interfaces/IWorkSessionServices.cs
@@ -6,7 +6,7 @@ namespace EntryLog.Business.Interfaces;
 
 public interface IWorkSessionServices
 {
-    Task<(bool success, string message)> OpenJobSessionAsync(CreateJobSessionDto sessionDto);
+    Task<(bool success, string message)> OpenJobSessionAsync(CreateWorkSessionDto sessionDto);
     Task<(bool success, string message)> ClosedJobSessionAsync(CloseJobSessionDto sessionDto);
     Task<PaginatedResult<GetWorkSessionDto>> GetSessionListByFilterAsync(WorkSessionQueryFilter filter);
 }
