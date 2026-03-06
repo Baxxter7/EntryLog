5002070c1cb5da60dc90d39258a9085c79c88533
Josue Martinez <josueisaiasmartinezlopez@gmail.com>
2026-03-05 23:42:12 -0600

    refactor: rename CreateWorkSessionDto

---
+++ EntryLog.Business/DTOs/CreateWorkSessionDto.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Http;
+
+namespace EntryLog.Business.DTOs;
+
+public record CreateWorkSessionDto(
+   string UserId,
+   string Latitude,
+   string Longitude,
+   IFormFile Image,
+   string? Notes,
+   string descriptor
+);
