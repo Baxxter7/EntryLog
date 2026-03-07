139037b43068be5b71aa6f104771fcb44711764b
Josue Martinez <josueisaiasmartinezlopez@gmail.com>
2026-03-06 22:38:23 -0600

    feat: add workSessionController

---
+++ EntryLog.Web/Controllers/WorkSessionController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace EntryLog.Web.Controllers;
+
+public class WorkSessionController : Controller
+{
+    public IActionResult Index()
+    {
+        return View();
+    }
+}
