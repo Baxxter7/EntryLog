a1ba2a13cf816c5a3e1e45b4e0900a8d38214358
Josue Martinez <josueisaiasmartinezlopez@gmail.com>
2026-03-08 21:26:16 -0600

    refactor: add service dependencies to WorkSessionController

--- EntryLog.Web/Controllers/WorkSessionController.cs
+++ EntryLog.Web/Controllers/WorkSessionController.cs
@@ -1,10 +1,20 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using EntryLog.Business.Interfaces;
+using Microsoft.AspNetCore.Mvc;
 
 namespace EntryLog.Web.Controllers;
 
 public class WorkSessionController : Controller
 {
-    public IActionResult Index()
+    private IWorkSessionServices _workSessionServices;
+    private IFaceIdService _faceId;
+
+    public WorkSessionController(IWorkSessionServices workSessionServices, IFaceIdService faceId)
+    {
+        _workSessionServices = workSessionServices;
+        _faceId = faceId;
+    }
+
+    public async Task <IActionResult> Index()
     {
         return View();
     }
