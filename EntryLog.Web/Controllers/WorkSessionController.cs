1e112170020effc953d0f510aaaad943df5d3a8b
Josue Martinez <josueisaiasmartinezlopez@gmail.com>
2026-03-09 23:32:15 -0600

    feat: implement workSessionController

--- EntryLog.Web/Controllers/WorkSessionController.cs
+++ EntryLog.Web/Controllers/WorkSessionController.cs
@@ -1,21 +1,38 @@
-﻿using EntryLog.Business.Interfaces;
+﻿using EntryLog.Business.DTOs;
+using EntryLog.Business.Interfaces;
+using EntryLog.Business.Pagination;
+using EntryLog.Business.QueryFilters;
+using EntryLog.Web.Extensions;
+using EntryLog.Web.Models;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 
 namespace EntryLog.Web.Controllers;
 
+[Authorize(Roles ="Employee")]
 public class WorkSessionController : Controller
 {
-    private IWorkSessionServices _workSessionServices;
-    private IFaceIdService _faceId;
+    private readonly IWorkSessionServices _workSessionService;
+    private readonly IFaceIdService _faceIdService;
 
     public WorkSessionController(IWorkSessionServices workSessionServices, IFaceIdService faceId)
     {
-        _workSessionServices = workSessionServices;
-        _faceId = faceId;
+        _workSessionService = workSessionServices;
+        _faceIdService = faceId;
     }
 
-    public async Task <IActionResult> Index()
+    public async Task<IActionResult> Index()
     {
-        return View();
+        UserViewModel userData = User.GetUserData()!;
+        EmployeeFaceIdDto faceId = await _faceIdService.GetFaceIdAsync(userData.NameIdentifier);
+        ViewBag.IsFaceIdActive = faceId.Active;
+
+        PaginatedResult<GetWorkSessionDto> model = await _workSessionService.GetSessionListByFilterAsync(new WorkSessionQueryFilter
+        {
+            EmployeeId = userData.NameIdentifier,
+            Sort = Business.Enums.SortType.Descending
+        });
+
+        return View(model);
     }
 }
