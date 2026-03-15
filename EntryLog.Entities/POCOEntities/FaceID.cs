7ef9d19fe3605f679beb249b9965f454a9b7f2cb
Josue Martinez <josueisaiasmartinezlopez@gmail.com>
2026-03-14 23:48:05 -0600

    refactor

--- EntryLog.Entities/POCOEntities/FaceID.cs
+++ EntryLog.Entities/POCOEntities/FaceID.cs
@@ -3,7 +3,7 @@
 public class FaceID
 {
     public string ImageUrl { get; set; } = string.Empty;
-    public DateTime RegisterDate {  get; set; }
+    public DateTime RegisterDate { get; set; }
     public List<float> Descriptor { get; set; } = [];
     public bool Active { get; set; }
 }
