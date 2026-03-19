6fb9963a3f7eb5be5dd65fd6ac1cf2ec23a4e9b0
Josue Martinez <josueisaiasmartinezlopez@gmail.com>
2026-03-18 23:14:35 -0600

    feat: add OpenWorkSessionViewModel

---
+++ EntryLog.Web/Models/OpenWorkSessionViewModel.cs
@@ -0,0 +1,9 @@
+﻿namespace EntryLog.Web.Models;
+
+public record OpenWorkSessionViewModel(
+    string Latitude,
+    string Longitude,
+    IFormFile Image,
+    string? Notes,
+    string Descriptor
+);
