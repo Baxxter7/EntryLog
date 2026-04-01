7249cbcfcd4c278ef27ff922d008e5c3a4c9f6b9
Josue Martinez <josueisaiasmartinezlopez@gmail.com>
2026-03-31 20:42:19 -0600

    feat: add CloseWorkSessionViewModel

---
+++ EntryLog.Web/Models/CloseWorkSessionViewModel.cs
@@ -0,0 +1,11 @@
+﻿namespace EntryLog.Web.Models
+{
+    public record CloseWorkSessionViewModel(
+        string sessionId,
+        string Latitude,
+        string Longitude,
+        IFormFile Image,
+        string? Notes,
+        string Descriptor
+    );
+}
