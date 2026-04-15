bb3c07a5344269f0a7378b81912b632b108bb8ce
Josue Martinez <josueisaiasmartinezlopez@gmail.com>
2026-04-14 21:14:22 -0600

    feat: add properties to location

--- EntryLog.Entities/POCOEntities/Location.cs
+++ EntryLog.Entities/POCOEntities/Location.cs
@@ -4,6 +4,9 @@ public class Location
 {
     public string Latitude { get; set; } = string.Empty;
     public string Longitude { get; set; } = string.Empty;
+    public string Country { get; set; } = string.Empty;
+    public string City { get; set; } = string.Empty;
+    public string Neighbourhood { get; set; } = string.Empty;
     public string? ApproximateAddress { get; set; }
     public string IpAddress { get; set; } = string.Empty;
 }
