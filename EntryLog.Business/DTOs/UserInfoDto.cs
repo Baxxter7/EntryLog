54328ef8de0bda46af81297ac9cf7f4560ccb22e
Josue Martinez <josueisaiasmartinezlopez@gmail.com>
2026-04-16 18:06:23 -0600

    feat:: UserInfoDto

---
+++ EntryLog.Business/DTOs/UserInfoDto.cs
@@ -0,0 +1,20 @@
+﻿namespace EntryLog.Business.DTOs;
+
+public record UserInfoDto
+(
+    string Code,
+    string Name,
+    string Role,
+    string Active,
+    string Position,
+    string PositionName,
+    string PositionDescription,
+    string Email,
+    string CellPhone, 
+    string DateOfBirthDay,
+    string ProfileImage,
+    string City,
+    bool IsFaceIdActive,
+    bool IsActive
+);
+
