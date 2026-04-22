65b558eeebb3b146b84876968e7836396686e6b6
Josue Martinez <josueisaiasmartinezlopez@gmail.com>
2026-04-21 23:09:33 -0600

    feat: return position information

--- EntryLog.Data/SqlLegacy/Repositories/EmployeeRepository.cs
+++ EntryLog.Data/SqlLegacy/Repositories/EmployeeRepository.cs
@@ -1,15 +1,17 @@
 ﻿using EntryLog.Data.Interfaces;
 using EntryLog.Data.SqlLegacy.Contexts;
 using EntryLog.Entities.POCOEntities;
+using Microsoft.EntityFrameworkCore;
 
-namespace EntryLog.Data.SqlLegacy.Repositories
+namespace EntryLog.Data.SqlLegacy.Repositories;
+internal class EmployeeRepository(EmployeesDbContext context) : IEmployeeRepository
 {
-    internal class EmployeeRepository(EmployeesDbContext context) : IEmployeeRepository
+    private readonly EmployeesDbContext _context = context;
+    public async Task<Employee?> GetByCodeAsync(int code)
     {
-        private readonly EmployeesDbContext _context = context;
-        public async Task<Employee?> GetByCodeAsync(int code)
-        {
-            return await _context.Employees.FindAsync(code);
-        }
+        return await _context.Employees
+            .Where(x  => x.Code == code)
+            .Include(e => e.Position)
+            .FirstOrDefaultAsync();
     }
 }
